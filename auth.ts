bbaffc95846d6ce92086c30495b5747aaff9109a
LordNtlaloe <ntlal0e182@gmail.com>
2024-09-28 23:13:41 +0200

    Feature: Added Login and Logout Functionality

--- auth.ts
+++ auth.ts
@@ -2,8 +2,12 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "./lib/prisma"
 import Google from "next-auth/providers/google"
+import logo from '@/public/logo.png'
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  theme:{
+    logo: `${logo}`
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
     Google
