2caee3323b8af064ed5120089b2f0ce42212204f
LordNtlaloe <ntlal0e182@gmail.com>
2024-09-28 23:44:04 +0200

    Feature: Added Protected Pages Functionality

--- app/(admin)/settings/actions.ts
+++ app/(admin)/settings/actions.ts
@@ -1,7 +1,30 @@
-// "use server"
+"use server";
 
-// import { UpdateProfileValues, UpdateProfileSchema } from "@/lib/validations";
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+import { UpdateProfileValues, updateProfileSchema } from "@/lib/validation";
+import { revalidatePath } from "next/cache";
 
-// export async function updateProfile(values: UpdateProfileValues) {
-//     const { name } = UpdateProfileSchema.parse(values);
-// }
+// To learn more about server actions, watch my YT tutorial: https://www.youtube.com/watch?v=XD5FpbVpWzk
+
+export async function updateProfile(values: UpdateProfileValues) {
+    const session = await auth();
+    const userId = session?.user?.id;
+
+    if (!userId) {
+        throw new Error("You Are Unauthorized")
+    }
+
+    const { name } = updateProfileSchema.parse(values);
+
+    await prisma.user.update({
+        where:{
+            id: userId
+        },
+        data: {
+            name
+        }
+    })
+    revalidatePath("/");
+
+}
--- app/(admin)/settings/page.tsx
+++ app/(admin)/settings/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Metadata } from "next";
+import SettingsPage from "./SettingsPage";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return (
-    <div>
-      
-    </div>
-  )
+export const metadata: Metadata = {
+  title: "Settings",
+};
+
+export default async function Page() {
+  const session = await auth();
+  const user = session?.user;
+  if(!user){
+    redirect("/api/auth/signin?callbackUrl=/settings")
+  }
+  return <SettingsPage user={user} />;
 }
