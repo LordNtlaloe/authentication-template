0c9f8b52bd2065502c3a8cd77f821cd481fdaf44
LordNtlaloe <ntlal0e182@gmail.com>
2024-09-28 20:44:38 +0200

    Feature: Set Next Auth & Prisma

---
+++ app/(admin)/settings/actions.ts
@@ -0,0 +1,7 @@
+// "use server"
+
+// import { UpdateProfileValues, UpdateProfileSchema } from "@/lib/validations";
+
+// export async function updateProfile(values: UpdateProfileValues) {
+//     const { name } = UpdateProfileSchema.parse(values);
+// }
