b0d64e9037f01b225f241cf300316ffd3d736a90
LordNtlaloe <ntlal0e182@gmail.com>
2024-09-28 20:44:38 +0200

    Feature: Set Next Auth & Prisma

---
+++ app/(admin)/admin/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: "Admin"
+}
+
+export default function Page() {
+  return (
+    <main className='mx-auto my-10 space-y-3'>
+      <h1 className="text-center text-xl font-bold">Admin Page</h1>
+      <p className="text-center">Welcome Admin</p>
+    </main>
+  )
+}
